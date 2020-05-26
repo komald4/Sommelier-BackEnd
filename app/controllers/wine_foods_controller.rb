ed4874d3e4973f0c5bf84824cbfd09632adac231
komald4 <komald.4@gmail.com>
2020-05-25 21:34:52 -0400

    Seed

--- app/controllers/wine_foods_controller.rb
+++ app/controllers/wine_foods_controller.rb
@@ -1,15 +1,15 @@
 class WineFoodsController < ApplicationController
 
     def index
-        food_wines = FoodWine.all
-        render json: food_wines, except: [:updated_at, :created_at],
+        wine_foods = WineFood.all
+        render json: wine_foods, except: [:updated_at, :created_at],
         include: [:food, :wine]
     end
 
     def destroy
-        food_wine = FoodWine.find(params[:id])
-        food_wine.destroy
-        render json: food_wine, except: [:updated_at, :created_at]
+        wine_foods = WineFood.find(params[:id])
+        wine_foods.destroy
+        render json: wine_foods, except: [:updated_at, :created_at]
     end
 
   
